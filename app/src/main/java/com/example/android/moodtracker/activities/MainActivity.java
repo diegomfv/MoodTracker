c531a0997118d2498b13a85a902ee95c6b1cb7f0
diegoma77 <31185512+diegoma77@users.noreply.github.com>
2018-03-28 16:03:44 +0100

    Create folders(packages)

--- app/src/main/java/com/example/android/moodtracker/activities/MainActivity.java
+++ app/src/main/java/com/example/android/moodtracker/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.android.moodtracker;
+package com.example.android.moodtracker.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.android.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
